0c5675bff955cc793ff177806dd2698bc78db00d
modify95 <modify95@naver.com>
2018-09-18 13:06:12 +0900

    detail js 최종 수정 완료

--- WebContent/scripts/detail.js
+++ WebContent/scripts/detail.js
@@ -36,23 +36,20 @@ function showMap() {
 
 function getDetail(id) {
 	var url = 'https://javascript-basic.appspot.com/locationDetail';
-
 	$.getJSON(url, {
 		id : id
 	}, function(r) {
-		$('detail-header-name').html(r.name);
-		$('detail-header-city-name').html(r.cityName);
-		$('detail-desc-text').html(r.desc);
-
+		$('.detail-header-name').html(r.name);
+		$('.detail-header-city-name').html(r.cityName);
+		$('.detail-desc-text').html(r.desc);
 		var $gallery = $('#detail-images');
 		var images = r.subImageList;
 		for (var i = 0; i < images.length; i++) {
-			var $image = $('<img src="' + images[i] + '"/>');
+			var $image = $('<img src="' + images[i] + '" />');
 			$gallery.append($image);
 		}
 		Galleria.loadTheme('libs/galleria/themes/classic/galleria.classic.min.js');
-		Galleria.run('detail-images');
-		showMarker(r.position.x, r.position.y);
+		Galleria.run('#detail-images');
 	});
 }
 
@@ -74,7 +71,7 @@ function parseId(str) {
 
 	for (var i = 0; i < args.length; i++) {
 		var arg = args[i];
-		var tokens = args.split('=');
+		var tokens = arg.split('=');
 
 		if (tokens[0] === 'id') {
 			return tokens[1];
